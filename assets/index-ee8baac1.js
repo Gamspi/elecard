9c3ceb559afc0fd4dbc0adc1953f551eeb44196b
Gamspi <niko.tatt007@gmail.com>
2023-09-18 00:13:21 +0700

    Updates

--- assets/index-ee8baac1.js
+++
@@ -1,2 +0,0 @@
-import{D as o,E as e,F as n}from"./index-dc745bb9.js";const t=o,r={Primary:e,Icon:n};export{r as C,t as u};
-//# sourceMappingURL=index-ee8baac1.js.map
---
+++ assets/ui-b58bbbce.js
@@ -0,0 +1,2 @@
+import{r as t,j as o,d as e,C as n}from"./index-54b2b1e8.js";const s=()=>o.jsx(e,{children:o.jsx(n,{children:"NotFoundPage"})}),a=t.memo(s);export{a as default};
+//# sourceMappingURL=ui-b58bbbce.js.map
