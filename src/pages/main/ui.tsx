80fd6fffb73ceaf1b13523df9f24f302f994c1de
Gamspi <niko.tatt007@gmail.com>
2023-09-18 23:03:06 +0700

    refactoring

--- src/pages/main/ui.tsx
+++ src/pages/main/ui.tsx
@@ -5,28 +5,24 @@ import Scrollbars from "react-custom-scrollbars-2"
 
 const MainPage = () => {
   return (
-    <Scrollbars>
-      <Page>
-        <StyledComponent.Container>
-          <Typography.Title level={1}>Hello friend 🖖</Typography.Title>
-          <Typography.Title level={5}>
-            I hope you like my project 😇
-          </Typography.Title>
-          <Typography.Title level={6}>Main stack:</Typography.Title>
-          <Typography.List>
-            <li>React</li>
-            <li>Axios</li>
-            <li>Type script</li>
-            <li>Redux-toolkit</li>
-            <li>Styled components</li>
-            <li>React transition group - for animation</li>
-          </Typography.List>
-          <Typography.Text>
-            * You need to go to the catalog page
-          </Typography.Text>
-        </StyledComponent.Container>
-      </Page>
-    </Scrollbars>
+    <Page>
+      <StyledComponent.Container>
+        <Typography.Title level={1}>Hello friend 🖖</Typography.Title>
+        <Typography.Title level={5}>
+          I hope you like my project 😇
+        </Typography.Title>
+        <Typography.Title level={6}>Main stack:</Typography.Title>
+        <Typography.List>
+          <li>React</li>
+          <li>Axios</li>
+          <li>Type script</li>
+          <li>Redux-toolkit</li>
+          <li>Styled components</li>
+          <li>React transition group - for animation</li>
+        </Typography.List>
+        <Typography.Text>* You need to go to the catalog page</Typography.Text>
+      </StyledComponent.Container>
+    </Page>
   )
 }
 
